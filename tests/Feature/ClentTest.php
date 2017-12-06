7439f67467b482a05dccde9c3849c55201ef29a7
karthikpuvvula <karthik.puvvula@gmail.com>
2017-12-06 14:44:57 +0530

    http test

--- tests/Feature/ClentTest.php
+++ tests/Feature/ClentTest.php
@@ -17,10 +17,39 @@ class ClentTest extends TestCase
         $this->assertTrue(true);
     }
 
-    public function testDatabase()
-    {
-    	$clients = factory('App\Client', 10)->create();
+    // public function testDatabase()
+    // {
+    // 	$clients = factory('App\Client', 10)->create();
+    // }
 
-    	$this->assertDatabaseHas('clients', $clients->toArray());
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testHttp()
+    {
+        $response = $this->json('POST', '/clients', 
+            [
+                'name' => 'abc',
+                'gender' => 'female',
+                'dob' => '1986-05-22',
+                'phone' => '9920656788',
+                'email' => 'pmohan_86@yahoo.com',
+                'address' => 'abc 123 3454546 465xfsfds ddgs',
+                'nationality' => 'India',
+                'education' => 'MBA',
+                'preferred_contact_mode' => 'none'
+        ]);
+
+        dd($response);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'created' => true,
+            ]);
     }
 }
